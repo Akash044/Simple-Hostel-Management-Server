79c3f28f08ff40d9ebffdf959f9491db57ea6209
Akash374 <akash.hossain@dataphilics.in>
2022-03-14 20:06:35 +0600

    gg

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const fileUpload = require('express-fileupload');
 const ObjectId = require('mongodb').ObjectID;
 const MongoClient = require('mongodb').MongoClient;
 const bcrypt = require('bcrypt');
-// const fs = require('fs-extra');
 const nodemailer = require('nodemailer');
 const saltRounds = 10;
 
@@ -41,7 +40,6 @@ client.connect(err => {
 
 
    app.post('/addGuest', (req, res) => {
-      // console.log(req.body)
       const guestImg = req.body.base64;
       const imgSize = req.body.fileSize;
       const type = req.body.type;
@@ -51,16 +49,10 @@ client.connect(err => {
       const date = req.body.today;
       const relativeEmail = req.body.relativeEmail;
 
-      // console.log(roomNo);s
-      // const newImg = boarderImg.data;
-      // const newImg = boarderImg; 
-      // const encodeImg = newImg.toString('base64');
-
       const image = {
          contentType: type,
          size: imgSize,
          img: Buffer.from(guestImg, 'base64')
-         // img:newImg 
       };
       const guestInfo = { name: name, mobile: mobile, address: address, date: date, relativeEmail: relativeEmail, ...image }
 
@@ -84,17 +76,12 @@ client.connect(err => {
          })
    })
 
-
-   //  ALL BOOKS SECTION START HERE
-   // read all boarders
    app.get('/boarders', (req, res) => {
-      // console.log('hi')
       boarderCollection.find({})
          .toArray((err, documents) => {
             res.send(documents);
          })
    })
-   // read boarder by email
    app.get('/boarder/:email', (req, res) => {
       const userEmail = req.params.email;
       boarderCollection.find({ email: userEmail })
@@ -133,9 +120,6 @@ client.connect(err => {
 
       bcrypt.genSalt(saltRounds, (err, salt) => {
          bcrypt.hash(password, salt, (err, hash) => {
-
-            // console.log(hash);
-            // Now we can store the password hash in db.
             boarderCollection.insertOne({ ...borderInfo, password: hash })
                .then(result => {
                   res.send(result.insertedCount > 0)
@@ -144,41 +128,22 @@ client.connect(err => {
       });
 
    })
-
-   // insert new boarder
    app.post('/uploadRoomImg', (req, res) => {
-      // console.log(); 
-
       const boarderImg = req.body.base64;
       const imgSize = req.body.fileSize;
       const type = req.body.type;
-      // console.log(boarderImg);
-      // const newImg = boarderImg.data;
-      // const newImg = boarderImg; 
-      // const encodeImg = newImg.toString('base64');
-
       const image = {
          contentType: type,
          size: imgSize,
          img: Buffer.from(boarderImg, 'base64')
-         // img:newImg
       };
       boarderCollection.insertOne(image)
          .then(result => {
-            // console.log("data added successfully");
             res.send(result.insertedCount > 0);
          })
    })
 
    app.patch('/paidRents/:id', (req, res) => {
-
-
-
-
-         console.log(req.body)
-      
-
-
 
       let message = '';
       let mailResponse = false;
@@ -186,7 +151,7 @@ client.connect(err => {
          message = `Your payment (TrxID: ${req.body.trxID}) is being processed. Please wait for it to be processed.
                  Thank you`
       } else if (req.body.status == "paid") {
-         message = `Your payment (TrxID: ${req.body.trxID}) is completed.
+         message = `Your payment (TrxID: ${req.body.trxID}) has been completed.
                  Thank you`
       } else {
          message = `Your payment (TrxID: ${req.body.trxID}) is unsuccessful. Please check the number and try again. If anything done wrong from you, I don't have any way to resolve this. Otherwise contact with me asap.
@@ -203,12 +168,12 @@ client.connect(err => {
             let transporter = nodemailer.createTransport({
                service: 'gmail',
                auth: {
-                  user: process.env.GMAIL,
-                  pass: process.env.PASS
+                  user: "ruetakash@gmail.com",
+                  pass: "1703044A"
                }
             });
             let mailOptions = {
-               from: process.env.GMAIL,
+               from:"ruetakash@gmail.com",
                to: req.body.email,
                subject: 'Rent Status',
                text: message
@@ -225,58 +190,17 @@ client.connect(err => {
                   mailResponse = true;
                   result.modifiedCount > 0 && res.send({ updateInfo: true, mailResponse })
                }
-
-               // if (result.modifiedCount > 0) {
-               //    res.send({ updateInfo: true, mailResponse })
-               // } else {
-               //    res.send({ updateInfo: true, mailResponse })
-               // }
-
-               // res.send(result.modifiedCount > 0)
             });
-
    }) 
 })
-
-      // //  delete a book
-      // app.delete('/deleteBook/:id', (req, res) => {
-      //    collection.deleteOne({ _id: ObjectId(req.params.id) })
-      //       .then(result => 
-      //          {
-      //             res.send(result.deletedCount > 0);
-      //          })
-      // })
-      // ALL BOOKS SECTION END HERE
-
-      // //ORDER SECTION START HERE
-      // // add order
-      // app.post('/addOrder', (req, res) => {
-      //    const orderInfo = req.body;
-      //    ordersCollection.insertOne(orderInfo)
-      //       .then(result => {
-      //          console.log("data added successfully");
-      //          res.send(result);
-      //       })
-      // })
-
-      // // read orders
-      // app.get('/allOrder/:email', (req, res) => {
-      //    ordersCollection.find({email: req.params.email })
-      //       .toArray((err, documents) => {
-      //          res.send(documents);
-      //       })
-      // })
-      // // ORDER SECTION END HERE
 
       app.get('/isAdmin', (req, res) => {
          adminCollection.find({ email: req.query.email })
             .toArray((err, documents) => {
-               // console.log(documents.length)
                res.send(documents.length > 0);
             })
       })
       app.post('/addAdmin', (req, res) => {
-         // console.log(req.body);
          adminCollection.insertOne(req.body)
             .then(result => {
                res.send(result.insertedCount > 0)
@@ -285,7 +209,6 @@ client.connect(err => {
 
 
       app.post('/addMeal', (req, res) => {
-         // console.log(req.body);
          mealCollection.insertOne(req.body)
             .then(result => {
                res.send(result.insertedCount > 0)
@@ -295,17 +218,13 @@ client.connect(err => {
       app.get('/boarderMeal/:email', (req, res) => {
          mealCollection.find({ email: req.params.email })
             .toArray((err, documents) => {
-               // console.log(documents)
                res.send(documents);
             })
       })
 
       app.post('/addRent', (req, res) => {
-         // console.log(req.body);
          rentCollection.insertOne(req.body)
             .then(result => {
-               // res.send(result.insertedCount > 0)
-
 
                if (result.insertedCount > 0) {
 
@@ -346,23 +265,17 @@ client.connect(err => {
       app.get('/paidRents/:email', (req, res) => {
          rentCollection.find({ email: req.params.email })
             .toArray((err, documents) => {
-               // console.log(documents)
                res.send(documents);
             })
       })
       app.get('/paidRents', (req, res) => {
          rentCollection.find({})
             .toArray((err, documents) => {
-               // console.log(documents)
                res.send(documents);
             })
       })
 
-
-      // room section
       app.post('/addRoom', (req, res) => {
-         // console.log(req.body.fileSize);
-
          const boarderImg = req.body.base64;
          const imgSize = req.body.fileSize;
          const type = req.body.type;
@@ -370,17 +283,10 @@ client.connect(err => {
          const seat = req.body.seat;
          const description = req.body.description;
          const vacantStatus = req.body.vacantStatus;
-
-         // console.log(roomNo);s
-         // const newImg = boarderImg.data;
-         // const newImg = boarderImg; 
-         // const encodeImg = newImg.toString('base64');
-
          const image = {
             contentType: type,
             size: imgSize,
             img: Buffer.from(boarderImg, 'base64')
-            // img:newImg
          };
          const roomInfo = { roomNo: roomNo, seat: seat, description: description, vacantStatus: vacantStatus, ...image }
 
@@ -390,10 +296,8 @@ client.connect(err => {
             })
       })
       app.get('/allRooms', (req, res) => {
-         // console.log("asche")
          roomCollection.find({})
             .toArray((err, documents) => {
-               // console.log(documents)
                res.send(documents);
             })
       })
@@ -419,16 +323,13 @@ client.connect(err => {
       })
 
       app.get('/allMeals', (req, res) => {
-         console.log("asche")
          mealCollection.find({})
             .toArray((err, documents) => {
-               // console.log(documents)
                res.send(documents);  
             })
       })
 
       app.patch('/bookedRoom', (req, res) => {
-         // console.log("okey  ", req.body)
          roomCollection.updateOne({ _id: ObjectId(req.body.id) },
             {
                $set: { vacantStatus: req.body.status }
@@ -442,7 +343,6 @@ client.connect(err => {
 
 
       app.post('/login', (req, res) => {
-         //  console.log("here ",req.body);
          const myPassword = req.body.password;
 
          adminCollection.find({ email: req.body.email })
